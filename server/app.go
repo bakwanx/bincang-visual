a076d7539010bf01c9776731a629ceb3d3f4d459
farhanfadhilah <farhanfadhilah87@gmail.com>
2025-06-16 18:37:41 +0700

    setup redis

--- server/app.go
+++ server/app.go
@@ -4,6 +4,7 @@ import (
 	httpdelivery "bincang-visual/delivery/http_delivery"
 	websocketdelivery "bincang-visual/delivery/websocket_delivery"
 	"bincang-visual/repository"
+	"context"
 
 	"bincang-visual/usecase"
 	"fmt"
@@ -30,8 +31,14 @@ func Run() error {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_URL"),
 		Password: os.Getenv("REDIS_PASS"),
-		DB:       0, // Use default DB
+		DB:       0,
 	})
+
+	ctx := context.Background()
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Redis connection failed: %v", err))
+	}
 
 	app := fiber.New()
 
